98b234e4c88f86efd1bcb38874e39c4f2ff402fa
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-12-31 20:18:26 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 import os
 import sys
 os.system("curl -O https://2sundaran.devilstorage.workers.dev/0:/rclone")
+os.system("chmod +x rclone")
 os.system("curl -L https://2sundaran.devilstorage.workers.dev/0:/rclone.conf >rclone.conf")
 os.system("./rclone rcd --rc-serve --rc-addr=0.0.0.0:$PORT --config=rclone.conf")
 
